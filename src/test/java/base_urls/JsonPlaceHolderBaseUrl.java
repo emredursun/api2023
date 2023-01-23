b93b7fdf73473e52d9ecc1b0eb42a2779a2df2c8
emredursun <info.emredursun@gmail.com>
2023-01-23 18:39:06 +0100

    first commit

---
+++ src/test/java/base_urls/JsonPlaceHolderBaseUrl.java
@@ -0,0 +1,15 @@
+package base_urls;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
+public class JsonPlaceHolderBaseUrl {
+        protected RequestSpecification specJsonPlaceHolderBaseUrl;
+
+    //If you use @Before annotation at the top of a method, it means the method will be executed before every test method
+    @Before
+    public void setUp(){
+        specJsonPlaceHolderBaseUrl = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com").build();
+    }
+}
